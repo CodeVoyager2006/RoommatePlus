975e5dc28bc5c3a35b688b85cc06497f4ee1f231
Minh Son Truong <mn581343@dal.ca>
2025-11-15 01:32:38 -0400

    Produced intial template

---
+++ RoommateApp/src/Machine.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Machine(){
+  return (
+    <div>
+      <h2>Machine Available</h2>
+      <p>Machine availability content.</p>
+    </div>
+  );
+}
