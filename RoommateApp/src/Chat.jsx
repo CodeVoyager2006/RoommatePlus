6ccaab964b66b7fd0001e9109c3b2296722a2aec
eunchanp <141085723+eunchanp@users.noreply.github.com>
2025-11-17 23:11:07 -0400

    commit to use on pc

--- RoommateApp/src/Chat.jsx
+++ RoommateApp/src/Chat.jsx
@@ -1,10 +1,27 @@
 import React from "react";
 
-export default function Chat(){
+export default function Chat() {
   return (
-    <div>
-      <h2>Chat</h2>
-      <p>Chat UI placeholder.</p>
+    <div class="chat">
+      <div class="chat-header">
+        <div class="header-image"></div>
+        <div class="members">
+          <div class="member"></div>
+          <div class="member"></div>
+          <div class="member"></div>
+        </div>
+      </div>
+
+      <div class="chat-logs">
+        <div class="message received">Hello! How are you?</div>
+        <div class="message sent">I'm good, thanks!</div>
+        <div class="message received">Are you coming today?</div>
+      </div>
+
+      <div id="input-chat">
+        <input type="text" placeholder="Type a message..."></input>
+        <button>Send</button>
+      </div>
     </div>
   );
 }
