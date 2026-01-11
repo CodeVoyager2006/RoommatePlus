37fa065da93869f7df8cffd6890f465cae781d62
Tom Le <dlee0130@student.monash.edu>
2026-01-11 11:37:55 +1100

    Connected Supabase to app
    Next step:
    - Create API requests to retrieve and upload data (using Supabase client for now, no authentication yet)

--- RoommateApp/src/Setting.jsx
+++ RoommateApp/src/Setting.jsx
@@ -1,6 +1,12 @@
-import React from "react";
+import { React, useEffect } from "react";
+import supabaseClient from "./config/supabaseClient";
 
 export default function Setting(){
+  useEffect(() => {
+    console.log('Supabase Client:', supabaseClient);
+    console.log('Supabase URL:', import.meta.env.VITE_SUPABASE_URL);
+  }, []);
+
   return (
     <div>
       <h2>Settings</h2>
